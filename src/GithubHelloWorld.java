5829674b6e19b3ceb33b1521feb54f4d7c4323f4
Karim <karimkk05@gmail.com>
2020-02-26 13:19:34 -0500

    printedNormalHelloWorld

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,12 +1,13 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
         
     }
 
     public static void helloWorld(){
         System.out.println("Hello World");
     }
+
 
 
 
